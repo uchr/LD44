dfc2fab2dcd6813626cd06ccb130937ebac27a63
Anton Tarasov <uchiro.me@gmail.com>
2019-04-27 21:07:54 +0700

    Two zones

--- LD44/Assets/Scripts/Levels.cs
+++ LD44/Assets/Scripts/Levels.cs
@@ -7,10 +7,6 @@ public class Levels : MonoBehaviour
     public GameObject treePrefab;
     
     private void Awake() {
-        for (int i = 0; i < 1000; ++i) {
-            var position = new Vector3(Random.Range(-100f * 2, 100f * 2), 0, Random.Range(-56.25f * 2, 56.25f * 2));
-            GameObject.Instantiate(treePrefab, position, Quaternion.identity); 
-        }
     }
 
     private void Update()
