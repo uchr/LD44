49dd777ecea1a2a094435be061e6472b147bd4cc
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 12:30:28 +0700

    Map

--- LD44/Assets/Scripts/GameLogic.cs
+++ LD44/Assets/Scripts/GameLogic.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class PlayerState {
+    public static int hp;
+    public static int oxygen;
+}
+
 public class GameLogic : MonoSingleton<GameLogic> {
     public int stage = 0;
 
