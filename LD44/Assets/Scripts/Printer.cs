4b7a64e3ec9f3cad35ff2c33871494f923361a84
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 18:06:38 +0700

    Printer

---
+++ LD44/Assets/Scripts/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Printer : MonoBehaviour {
+    public string desc;
+    public bool isInit = false;
+
+    public GameObject textGO;
+    public TextMeshPro text;
+
+    private bool isActive = false;
+
+    private void Enter() {
+        isActive = true;
+        text.text = desc;
+        textGO.SetActive(true);
+    }
+
+    private void Exit() {
+        isActive = false;
+        textGO.SetActive(false);
+    }
+
+    private void Awake() {
+        GetComponentInChildren<Trigger>().enter.AddListener(Enter);
+        GetComponentInChildren<Trigger>().exit.AddListener(Exit);
+    }
+}
---
+++ LD44/Assets/Scripts/TextHide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextHide : MonoBehaviour {
+    public bool isInitText = true;
+
+    private void Update() {
+        if (isInitText && Input.anyKeyDown) {
+            isInitText = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
