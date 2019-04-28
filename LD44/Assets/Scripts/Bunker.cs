c0d33e7c7966502869acbe809a89f12f2f8626dc
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 15:12:23 +0700

    Assets in bunker

--- LD44/Assets/Scripts/Bunker.cs
+++ LD44/Assets/Scripts/Bunker.cs
@@ -8,20 +8,10 @@ public static class BunkerState {
 }
 
 public class Bunker : MonoBehaviour {
-    public GameObject bed;
-    public GameObject toilet;
-    public GameObject books;
-
-    public TextMeshPro listOfItems;
-
-    private void OnEnable() {
-        bed.SetActive(BunkerState.items.ContainsKey("Bed"));
-        toilet.SetActive(BunkerState.items.ContainsKey("Toilet"));
-        books.SetActive(BunkerState.items.ContainsKey("Books"));
-
+    /*private void OnEnable() {
         listOfItems.text = "";
         foreach (var item in BunkerState.items) {
             listOfItems.text += "<b>" + item.Key + "</b> " + item.Value.ToString() + "\n";
         }
-    }
+    }*/
 }
