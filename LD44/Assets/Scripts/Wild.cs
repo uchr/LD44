a28e6ce924a0bbbcae5342adf6007ee45f4ad75c
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 11:02:47 +0700

    Quest items

--- LD44/Assets/Scripts/Wild.cs
+++ LD44/Assets/Scripts/Wild.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wild : MonoBehaviour {
-    public GameObject[] stage0;
-    public GameObject[] stage1;
-    public GameObject[] stage2;
+    public GameObject[] stages;
     
-    public List<GameObject[]> items = new List<GameObject[]>();
+    public List<List<GameObject>> items = new List<List<GameObject>>();
 
     private void Awake() {
-        items.Add(stage0);
-        items.Add(stage1);
-        items.Add(stage2);
+        for (int i = 0; i < stages.Length; ++i) {
+            List<GameObject> stageItems = new List<GameObject>();
+            foreach (var item in stages[i].GetComponentsInChildren<Item>()) {
+                stageItems.Add(item.gameObject);
+                item.gameObject.SetActive(false);
+            }
+            items.Add(stageItems);
+        }
     }
 
     private void OnEnable() {
@@ -22,6 +25,11 @@ public class Wild : MonoBehaviour {
     }
 
     private void OnDisable() {
+        int stage = GameLogic.instance.stage;
+        foreach (var go in items[stage])
+            if (go != null)
+                go.SetActive(false);
+
         GameLogic.instance.stage++;
     }
 }
