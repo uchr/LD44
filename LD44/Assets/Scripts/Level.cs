d626ebca5082f4e53961006beb970757d1121078
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 14:52:32 +0700

    Enter to bunkers

--- LD44/Assets/Scripts/Level.cs
+++ LD44/Assets/Scripts/Level.cs
@@ -3,11 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour {
-    public Transform spawnPoint;
     public GameObject player;
-
-    private void OnEnable() {
-        player.transform.position = spawnPoint.position;
-
-    }
 }
