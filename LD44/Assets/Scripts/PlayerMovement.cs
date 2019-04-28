9dc309e81d62a03f2f11bb1436f17a9321ef3567
Anton Tarasov <uchiro.me@gmail.com>
2019-04-28 15:40:56 +0700

    Update bunkers and map

--- LD44/Assets/Scripts/PlayerMovement.cs
+++ LD44/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
 
     private Rigidbody rb;
+    private Animator animator;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update() {
+        animator.SetBool("walk", Mathf.Abs(rb.velocity.z) > 0.01f);
         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
     }
 }
