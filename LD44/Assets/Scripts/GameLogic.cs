ea74cfa85f998f57ca02354979c6cf4609634a9a
Anton Tarasov <uchiro.me@gmail.com>
2019-04-30 08:46:10 +0700

    3D printer and rocket

--- LD44/Assets/Scripts/GameLogic.cs
+++ LD44/Assets/Scripts/GameLogic.cs
@@ -6,29 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameLogic : MonoSingleton<GameLogic> {
     public GameObject winGame;
     public GameObject endGame;
-    public GameObject homeBunker;
+    public GameObject rocket;
 
-    //private void Awake() {
-    //    Time.timeScale = 1.0f;
-    //}
+    private bool isEnd = false;
+
+    private void Start() {
+        var begin = Stem.SoundManager.GrabSound("Begin");
+        begin.Play();
+        Stem.MusicManager.Play("Music");
+    }
 
     private void Update() {
-        /*if (PlayerState.instance.curHP <= 0.0f) {
-            endGame.SetActive(true);
-            Time.timeScale = 0.0f;
-            if (Input.anyKeyDown) {
-                SceneManager.LoadScene("Main", LoadSceneMode.Single);
-            }
-        }*/
-
-        /*if (WaitItemQuest.instance.isEnd && 
+        if (WaitItemQuest.instance.isEnd && 
             PlaceItemQuest.instance.isEnd &&
-            CollectItemsQuest.instance.isEnd) {
-            winGame.SetActive(true);
-            Time.timeScale = 0.0f;
-            if (Input.anyKeyDown) {
-                SceneManager.LoadScene("Main", LoadSceneMode.Single);
-            }
-        }*/
+            CollectItemsQuest.instance.isEnd && !isEnd) {
+                isEnd = true;
+                rocket.SetActive(false);
+                var end = Stem.SoundManager.GrabSound("Rocket");
+                end.Play();
+        }
     }
 }
